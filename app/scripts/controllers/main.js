788b3438a2c4c244de7bb9d95da6494fca2fbcd2
Joe Gesualdo <joegesualdo@gmail.com>
2015-09-02 17:27:23 -0400

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name vocabspreeFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the vocabspreeFrontendApp
+ */
+angular.module('vocabspreeFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
